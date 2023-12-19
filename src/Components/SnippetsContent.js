f8f0ec0ca59910fd8ecfe7a3ec5398fcdbc3ad0a
Edin Muhic <edin.muhic@forsta.com>
2023-12-19 14:30:18 +0100

    Add basic outline for snippet body

--- src/Components/SnippetsContent.js
+++ src/Components/SnippetsContent.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import '../Styles/SnippetContent.css';
 
 function SnippetsContent() {
     const snippets = [
-        { id: 1, content: "Snippet 1 content..." },
-        { id: 2, content: "Snippet 2 content..." },
+        { id: 1, title: "Snippet Title 1", content: "Snippet 1 content..." },
+        { id: 2, title: "Snippet Title 2", content: "Snippet 2 content..." },
     ];
 
     return (
-        <div>
+        <div className="snippets-container">
             {snippets.map(snippet => (
-                <div key={snippet.id} style={{ width: '300px', margin: '10px' }}>
-                    {snippet.content}
+                <div key={snippet.id} className="snippet">
+                    <h3 className="snippet-title">{snippet.title}</h3>
+                    <p className="snippet-content">{snippet.content}</p>
                 </div>
             ))}
         </div>
