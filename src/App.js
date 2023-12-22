9c217fbda49c6dede91ede0c470ece6bbe82eb90
Edin Muhic <edin.muhic@forsta.com>
2023-12-22 15:25:57 +0100

    Finish functionality

--- src/App.js
+++ src/App.js
@@ -7,26 +7,36 @@ import GridLayout from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
+function calculateSnippetHeight(snippet) {
+  const tempDiv = document.createElement('div');
+  tempDiv.style.width = '300px';
+  tempDiv.style.visibility = 'hidden';
+  tempDiv.innerHTML = `<div class='snippet-content'>${snippet.code}</div>`;
+
+  document.body.appendChild(tempDiv);
+
+  const height = tempDiv.offsetHeight;
+
+  document.body.removeChild(tempDiv);
+
+  return height;
+}
+
 function App() {
   const [gridWidth, setGridWidth] = useState(window.innerWidth);
-  const gridRef = useRef(null);
-
+  const rowHeight = 60;
   useEffect(() => {
-    // Function to update width
-    const updateWidth = () => {
-        if (gridRef.current) {
-            setGridWidth(gridRef.current.offsetWidth);
-        } else {
-            setGridWidth(window.innerWidth);
-        }
-    };
+    const updatedSnippets = snippets.map(snippet => {
+        const height = calculateSnippetHeight(snippet);
+        const gridHeight = Math.ceil(height / rowHeight);
 
-    // Update width on mount and window resize
-    window.addEventListener('resize', updateWidth);
-    updateWidth();
+        return {
+            ...snippet,
+            h: Math.max(gridHeight, 2)
+        };
+    });
 
-    // Cleanup
-    return () => window.removeEventListener('resize', updateWidth);
+    setSnippets(updatedSnippets);
 }, []);
 
   const [snippets, setSnippets] = useState([
@@ -69,7 +79,23 @@ function App() {
         isFavorite: false,
         url: "https://forsta.atlassian.net/wiki/spaces/FEH/pages/3234695260/How+to+use+Laptop+with+remote+k8s+environment",
         x: 8, y: 0, w: 3, h: 2
-    }
+    },
+    {
+        id: "6",
+        "code": "The <b>System.OutOfMemoryException you're encountering in your application, particularly when uploading an Excel file and using ClosedXML</b>, suggests that your application is running out of memory. This can be due to several reasons, especially when dealing with large Excel files. Here are some steps you can take to troubleshoot and potentially resolve this issue:<div>\n<b>Check File Size and Content</b>:\nDetermine the size of the Excel file you're trying to upload. Large files can consume a lot of memory, especially if they contain complex data, formulas, or images.\nIf possible, try uploading a smaller or simpler Excel file to see if the issue persists.</div><div>\n<b>Optimize Memory Usage</b>:</div><div>Ensure that your application is efficiently using memory. This includes disposing of objects that are no longer needed and avoiding unnecessary data duplication.\nIf you're processing the file in parts (like reading row by row), make sure you're not keeping references to objects longer than necessary.</div><div>\n<b>Increase Available Memory</b>:\nIf you're running your application in a constrained environment (like a server with limited resources or a 32-bit process), consider increasing the available memory or moving to a 64-bit environment.\nFor web applications, consider adjusting the memory limits in the application pool (if applicable).</div><div>\n<b>Use Streaming APIs</b>:\nIf ClosedXML is causing memory issues due to its in-memory model, consider using a more stream-based approach to handle Excel files, like EPPlus or NPOI, which can be more memory-efficient for large files.</div><div>\n<b>Check for Memory Leaks</b>:\nEnsure that your application doesn't have memory leaks. Tools like Visual Studio’s diagnostic tools, dotMemory, or a profiler can help identify leaks.</div><div>\n<b>Update Libraries</b>:\nMake sure you are using the latest version of ClosedXML, as newer versions might have optimizations or bug fixes that reduce memory usage.</div><div>\n<b>Split the File</b>:\nIf the file is exceptionally large, consider splitting it into smaller parts before processing.</div><div>\n<b>Server Configuration</b>:\nCheck the server configuration where the application is hosted. Ensure it has enough physical memory and is configured to allocate sufficient memory to your application.</div><div>\n<b>Error Handling and Logging</b>:\nImplement robust error handling and logging to capture more details when the exception occurs. This can provide insights into the specific operations that are leading to high memory usage.</div><div>\n<b>Consult Documentation and Community</b>:\nReview the documentation of ClosedXML for any recommendations on handling large files.\nConsider reaching out to the ClosedXML community or forums for advice. Others might have encountered and solved similar issues.\nIf after trying these steps the issue persists, you might need to consider an alternative approach to processing the Excel file or consulting with a developer experienced in handling large datasets and memory management in .NET applications.<br></div>",
+        "isFavorite": false,
+        "name": "Out of memory exception",
+        "url": "",
+        x: 10, y: 0, w: 3, h: 2
+      },
+      {
+        id: "7",
+        code: "account and password",
+        isFavorite: false,
+        name: "Netflix account and password",
+        url : "",
+        x: 10, y: 0, w: 3, h: 2
+      },
 ]);
 
 
@@ -88,7 +114,7 @@ const onLayoutChange = (newLayout) => {
           ...snippet,
           x: layoutItem.x,
           y: layoutItem.y,
-          w: Math.max(layoutItem.w, 3),
+          w: layoutItem.w,
           h: layoutItem.h
       };
   });
@@ -103,14 +129,14 @@ return (
                 className="layout"
                 layout={layout}
                 cols={12}
-                rowHeight={30}
+                rowHeight={rowHeight}
                 width={gridWidth}
                 onLayoutChange={onLayoutChange}
+                isResizable={true}
             >
-                {snippets.map(snippet => (
+                 {snippets.map(snippet => (
                     <div key={snippet.id}>
-                        <h3>{snippet.name}</h3>
-                        <p>{snippet.code}</p>
+                        <SnippetsContent snippet={snippet} />
                     </div>
                 ))}
             </GridLayout>
