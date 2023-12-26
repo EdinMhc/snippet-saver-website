bf8555a8aaa0976c32f2ef8e43ee66e6ee0478df
Edin Muhic <EdinAppleID@hotmail.com>
2023-12-27 00:30:36 +0100

    handleImport temp commit

--- src/Utils/layoutUtils/handleImport.js
+++ src/Utils/layoutUtils/handleImport.js
@@ -1,22 +1,34 @@
-export function handleImports(setSnippets){
-    return (file) => {
-        const reader = new FileReader();
-        reader.onload = (event) => {
+export function handleImports(setSnippets) {
+  return (file) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
           try {
-            const importedSnippets = JSON.parse(event.target.result);
-            const updatedSnippets = importedSnippets.map((snippet, index) => ({
-              ...snippet,
-              id: snippet.id || `snippet-${index}`,
-              x: index % 4,
-              y: Math.floor(index / 4),
-              w: 3,
-              h: 2,
-            }));
-            setSnippets(updatedSnippets);
+              const importedSnippets = JSON.parse(event.target.result);
+              setSnippets((currentSnippets) => {
+                  const uniqueImportedSnippets = importedSnippets.filter(importedSnippet => 
+                      !currentSnippets.some(currentSnippet => 
+                          currentSnippet.name === importedSnippet.name
+                      )
+                  );
+                  
+                  const maxX = currentSnippets.reduce((max, snippet) => Math.max(max, snippet.x), 0);
+
+                  // Add unique imported snippets to the right of existing snippets
+                  const updatedImportedSnippets = uniqueImportedSnippets.map((snippet, index) => ({
+                      ...snippet,
+                      id: snippet.id || `imported-snippet-${Date.now()}-${index}`,
+                      x: 0, // Place imported snippets to the right of existing ones
+                      y: 0, // Start at the top row
+                      w: snippet.w || 3,
+                      h: snippet.h || 2,
+                  }));
+
+                  return [...currentSnippets, ...updatedImportedSnippets];
+              });
           } catch (error) {
-            console.error("Error parsing JSON:", error);
+              console.error("Error parsing JSON:", error);
           }
-        };
-        reader.readAsText(file);
       };
-} 
+      reader.readAsText(file);
+  };
+}
