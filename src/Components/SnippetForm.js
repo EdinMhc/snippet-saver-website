dccd71dc75ac64cc796b11b1d91de9761a042243
Edin Muhic <EdinAppleID@hotmail.com>
2023-12-27 00:04:55 +0100

    Make form css look better

--- src/Components/SnippetForm.js
+++ src/Components/SnippetForm.js
@@ -16,35 +16,39 @@ function SnippetForm({ onAdd, onCancel }) {
 
   return (
     <div className="snippet-form-container">
+      <div className="snippet-whitespace">
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Code:</label>
-          <textarea
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>URL:</label>
-          <input
-            type="text"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-          />
+        <div className="snippet-form-content">
+          <div>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder='Name'
+            />
+          </div>
+          <div>
+            <textarea
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              placeholder='Content'
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              placeholder='Url'
+            />
+          </div>
         </div>
         <div className="form-actions">
           <button type="submit">Add</button>
           <button type="button" onClick={onCancel}>Cancel</button>
         </div>
       </form>
+      </div>
     </div>
   );
 }
