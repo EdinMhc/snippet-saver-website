221e70f579e5bd33218e7a00d4a82af311c76b93
Edin Muhic <EdinAppleID@hotmail.com>
2023-12-26 20:44:53 +0100

    Sort snippets when adding a new one

--- src/Utils/layoutUtils/handleAddSnippet.js
+++ src/Utils/layoutUtils/handleAddSnippet.js
@@ -1,37 +1,17 @@
 export function handleAddSnippet(snippets, setSnippets,setShowForm){
-    const findAvailablePosition = () => {
-        let x = 0, y = 0;
-        while (true) {
-          let found = true;
-          for (let snippet of snippets) {
-            if (snippet.x === x && snippet.y === y) {
-              found = false;
-              break;
-            }
-          }
-          if (found) return { x, y };
-          x += 2;
-          if (x >= 12) {
-            x = 0;
-            y += 2;
-          }
-        }
-      };
-
     return (newSnippet) => {
-        const position = findAvailablePosition();
-        const newSnippets = [
-            ...snippets,
-            {
-              ...newSnippet,
-              id: `snippet-${snippets.length + 1}`,
-              x: position.x,
-              y: position.y,
-              w: 2,
-              h: 2,
-            },
-          ];
-          setSnippets(newSnippets);
-          setShowForm(false);
+      const updatedSnippets = snippets.map(snippet => {
+          return { ...snippet, x: snippet.x + 2 };
+        });
+      const newSnippetWithPosition = {
+        ...newSnippet,
+        id: `snippet-${snippets.length + 1}`,
+        x: 0,
+        y: 0,
+        w: 2,
+        h: 2,
+        };
+        setSnippets([newSnippetWithPosition, ...updatedSnippets]);
+        setShowForm(false);
         };
 }
