a346351991fbd122e484dd4c4b17976dbf733b9a
Edin Muhic <edin.muhic@forsta.com>
2024-01-03 16:24:01 +0100

    install and implement react beautiful

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import NavBar from './Components/NavBar';
 import SnippetsContent from './Components/SnippetsContent';
 import FooterNotes from './Components/FooterNotes';
@@ -8,9 +9,20 @@ import SnippetForm from './Components/SnippetForm';
 import './Styles/App.css';
 
 function App() {
-  const windowSize = useWindowSize();
   const [snippets, setSnippets] = useState([]);
   const [showForm, setShowForm] = useState(false);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = Array.from(snippets);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setSnippets(items);
+  };
 
   return (
     <>
@@ -26,13 +38,33 @@ function App() {
           />
         )}
       </div>
-      <div className="App">
-        {snippets.map(snippet => (
-          <SnippetsContent key={snippet.id} snippet={snippet} />
-        ))}
+      <div className='outline-snippet-container'>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div className="App" {...provided.droppableProps} ref={provided.innerRef}>
+              {snippets.map((snippet, index) => (
+                <Draggable key={snippet.id} draggableId={snippet.id} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <SnippetsContent snippet={snippet} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
       </div>
       <FooterNotes />
     </>
   );
 }
+
 export default App;
